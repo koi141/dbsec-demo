c06c4b95eb7de1a3227bcb4357a723cbe9d50825
koiishii <ko1ro.mkitlo4@gmail.com>
2024-11-21 14:51:49 +0000

    pluginを追加

--- source/conf.py
+++ source/conf.py
@@ -13,7 +13,7 @@ author = 'koin3z'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = ['sphinxcontrib.seqdiag']
 
 templates_path = ['_templates']
 exclude_patterns = []
