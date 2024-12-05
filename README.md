afe721a3f3a07783d989839a7ceb8ea7cc4df7d0
koiishii <koichiro.ishii@oracle.com>
2024-12-05 14:43:49 +0000

    READMEを編集

--- README.md
+++ README.md
@@ -4,14 +4,3 @@
 ```
 https://koi141.github.io/dbsec-demo/
 ```
-
-以下のコマンドを実行する
-```
-pip install sphinx
-```
-
-```
-pip install sphinxcontrib-seqdiag
-pip install sphinx-book-theme
-```
-
--- source/conf.py
+++ source/conf.py
@@ -23,5 +23,6 @@ language = 'ja'
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_book_theme'
+# html_theme = 'sphinx_book_theme'
+html_theme = 'furo'
 html_static_path = ['_static']
